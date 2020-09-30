1078d982d994f60dbd2c698ab8e23248d0b3b7d6
plat-26 <lolade.badmus97@gmail.com>
2020-09-30 13:28:24 +0100

    added menu

--- BudgetManager.java
+++ BudgetManager.java
@@ -2,36 +2,107 @@ package budget;
 import  java.util.Scanner;
 
 
-
-public class BudgetManager {
+public class Main {
     public static void main(String[] args) {
-        new MoneyCount().getSum();
+
+        chooseAction();
+    }
+
+    static void showMenu() {
+        System.out.println("1) Add income");
+        System.out.println("2) Add purchase");
+        System.out.println("3) Show list of purchases");
+        System.out.println("4) Balance");
+        System.out.println("0) Exit");
+    }
+
+    static void chooseAction() {
+        Scanner scanner = new Scanner(System.in);
+        showMenu();
+        int x = scanner.nextInt();
+        System.out.println();
+        switch (x) {
+            case 1:
+                Income.addIncome();
+                System.out.println();
+                chooseAction();
+                break;
+            case 2:
+                System.out.println(Purchase.addPurchase());
+                System.out.println();
+                chooseAction();
+                break;
+            case 3:
+                System.out.println(Purchase.getList());
+                System.out.println();
+                chooseAction();
+                break;
+            case 4:
+                System.out.println(Income.getBalance());
+                System.out.println();
+                chooseAction();
+                break;
+            case 0:
+                exit();
+                break;
+            default:
+                chooseAction();
+        }
+    }
+
+    static void exit() {
+        System.out.println("Bye!");
     }
 }
 
-class MoneyCount {
+class Income {
 
-    public double sum;
+    static double balance;
 
-    public void getSum() {
-       System.out.println(getInput());
-       System.out.println( "Total: $" + sum);
-    }
-
-    private String getInput() { //get list input from console
+    static void addIncome() {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder budgetList = new StringBuilder();
-        String line;
-        while(scanner.hasNext()) {
-            line = scanner.nextLine();
-            budgetList.append(line + "\n");
-            calculateSum(line);
-        }
-        return budgetList.toString();
+        System.out.println("Enter Income");
+        balance += scanner.nextDouble();
+        System.out.println("Income was added");
     }
 
-    private void calculateSum(String line) { //calculate total dollars on list
+    static String getBalance() {
+       return "Balance: $" + balance;
+    }
+
+    static void updateBalance(Double price) {
+        balance -= price;
+    }
+}
+
+class Purchase {
+
+    private static double sum;
+    static StringBuilder list = new StringBuilder();
+
+    static String getList() {
+        if(!(list.toString()).isEmpty()) {
+            return list.toString() + "Total sum: $" + sum + "\n";
+        }
+        return "Purchase list is empty";
+    }
+
+    private static void calculateSum(String line) { //calculate total dollars on list
         sum += Double.parseDouble(line.substring(line.indexOf("$") + 1));
     }
 
+    public static String addPurchase() { //get list input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter purchase name:");
+            list.append(scanner.nextLine());
+            System.out.println("Enter its price:");
+            String price = scanner.nextLine();
+            list.append(" $" + price + "\n");
+            calculateSum(price);
+            Income.updateBalance(Double.parseDouble(price));
+
+        return "Purchase was added";
+    }
+
 }
+
